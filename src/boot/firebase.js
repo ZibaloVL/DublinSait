5851e0c2486bf0cd66aad67566d0ea860df21bea
ZibaloV <fotoroom.md@gmail.com>
2020-01-27 09:19:54 +0200

    remove config Keys from git

--- src/boot/firebase.js
+++ src/boot/firebase.js
@@ -1,15 +1,5 @@
 import firebase from 'firebase'
-
-const firebaseConfig = {
-  apiKey: 'AIzaSyARuVr5hu2BMHJ57bhcMh73_U6Nx0qazCA',
-  authDomain: 'dublinirishpubchishinau.firebaseapp.com',
-  databaseURL: 'https://dublinirishpubchishinau.firebaseio.com',
-  projectId: 'dublinirishpubchishinau',
-  storageBucket: 'dublinirishpubchishinau.appspot.com',
-  messagingSenderId: '834206533134',
-  appId: '1:834206533134:web:a405a39e608c9a60639e5f',
-  measurementId: 'G-ZFJ1SRH8JB'
-}
+import firebaseConfig from './configKeys/firebaseConfig'
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig)
