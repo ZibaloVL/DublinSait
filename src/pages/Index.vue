9bb22b2e9efcac9a55ea49d43d815eba9342f3e3
ZibaloV <fotoroom.md@gmail.com>
2020-02-05 11:13:06 +0200

    start make mapDialog

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -8,17 +8,20 @@
 <div>
   <main-carusel></main-carusel>
   <dialog-menu></dialog-menu>
+  <dialog-map></dialog-map>
 </div>
 </template>
 
 <script>
 import MainCarusel from '../components/mainCarusel'
 import DialogMenu from '../components/dialogMenu'
+import DialogMap from '../components/dialogMap'
 export default {
   name: 'PageIndex',
   components: {
     MainCarusel,
-    DialogMenu
+    DialogMenu,
+    DialogMap
   }
 }
 </script>
