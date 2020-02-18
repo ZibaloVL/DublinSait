27c0b380f1473f48b1f8e2e0ed87cb6b57687f78
ZibaloV <fotoroom.md@gmail.com>
2020-02-18 09:18:35 +0200

    begin change conyact

--- src/components/dialogContact.vue
+++ src/components/dialogContact.vue
@@ -2,30 +2,54 @@
   <div class="q-pa-md q-gutter-sm">
     <q-dialog
       v-model="icon"
-      full-width
-      full-height
     >
       <q-card>
-        <q-card-section
-          style="height: 100%"
+        <q-tabs
+          v-model="tab"
+          dense
+          class="text-grey"
+          active-color="primary"
+          indicator-color="primary"
+          align="justify"
+          narrow-indicator
         >
-          <iframe
-            src="https://www.google.com/maps/embed?pb=!4v1581923663655!6m8!1m7!1sCAoSK0FGMVFpcE9RZ1FFUkh4UGlmM0E0YWV5RElsdmh1Q1JHZXljYnVjX09nX0U.!2m2!1d47.0150541!2d28.8362359!3f181.06638378289003!4f0!5f0.7820865974627469"
-            width="100%"
-            height="100%"
-            frameborder="0"
-            style="border:0;"
-            allowfullscreen=""
+          <q-tab
+            name="mails"
+            label="Mails"
           />
-          <q-btn
-            v-close-popup
-            class="fixed-top-right, absolute-top-right"
-            icon="close"
-            flat
-            round
-            dense
+          <q-tab
+            name="alarms"
+            label="Alarms"
           />
-        </q-card-section>
+          <q-tab
+            name="movies"
+            label="Movies"
+          />
+        </q-tabs>
+        <q-separator />
+        <q-tab-panels
+          v-model="tab"
+          animated
+        >
+          <q-tab-panel name="mails">
+            <div class="text-h6">
+              Mails
+            </div>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          </q-tab-panel>
+          <q-tab-panel name="alarms">
+            <div class="text-h6">
+              Alarms
+            </div>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          </q-tab-panel>
+          <q-tab-panel name="movies">
+            <div class="text-h6">
+              Movies
+            </div>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          </q-tab-panel>
+        </q-tab-panels>
       </q-card>
     </q-dialog>
   </div>
@@ -35,7 +59,8 @@ import { mapGetters, mapMutations } from 'vuex'
 export default {
   data () {
     return {
-      icon: false
+      icon: false,
+      tab: 'mails'
     }
   },
   computed: {
@@ -53,7 +78,7 @@ export default {
   },
   methods: {
     ...mapMutations({
-      set_tourOnOff: 'set_tourOnOff'
+      set_contactOnOff: 'set_contactOnOff'
     })
   }
 }
